4b1ea9035363499d1e88ce9cd5dabc1032cac2c4
nitro-neal <groessler24@gmail.com>
2020-03-04 16:55:59 -0600

    Adding forever to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,14 @@ mkdir CIRCLE-CI-MYPASS-BACKEND || echo Directory Exists, Continuing...
 
 
 cd CIRCLE-CI-MYPASS-BACKEND
-git clone https://github.com/nitro-neal/mypass-backend.git
+git clone https://github.com/nitro-neal/mypass-backend.git || echo Already Cloned, Continuing...
+
+cd mypass-backend
+git pull
+
+yarn
+
+forever stop --uid "backend"
+forever start --uid "backend" app.js
 
 echo ~~FINISHED~~
