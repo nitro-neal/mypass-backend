9cf7d79faef956456e82c1dc6c3e0de5aef52fb9
nitro-neal <groessler24@gmail.com>
2020-03-04 14:59:07 -0600

    Adding circle ci deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cd /~
+mkdir CIRCLE-CI-MYPASS-BACKEND || echo Directory Exists, Continuing...
+
+
+cd CIRCLE-CI-MYPASS-BACKEND
+git clone https://github.com/nitro-neal/mypass-backend.git
+
+echo ~~FINISHED~~
